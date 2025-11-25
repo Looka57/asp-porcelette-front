2eb466e1d0b7bd9354566340b162892888fc85b4
Looka <napoa322@gmail.com>
2025-11-25 19:13:23 +0100

    feat: Ajout de la configuration Nginx et mise à jour de la gestion des requêtes API dans Axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,26 @@
-// src/api/axios.js
 import axios from 'axios';
 
-// URL de base de votre API C# (port 5067 dans vos captures)
+// Détection Docker vs local
+const isDocker = window.location.hostname !== 'localhost';
+const baseURL = isDocker
+  ? 'http://backend:8080/api/'   // FRONTEND -> BACKEND (réseau Docker)
+  : 'http://localhost:5067/api/'; // PC local (BDD pleine)
+
+
 const api = axios.create({
-    baseURL: 'http://localhost:5067/api/',
-    
+  baseURL,
 });
 
-// INTERCEPTEUR : Ajoute automatiquement le JWT
-api.interceptors.request.use(config => {
-    // Récupère le jeton que vous stockez après la connexion
+// Intercepteur JWT
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem('jwt_token');
-
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-}, error => {
-    return Promise.reject(error);
-});
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
